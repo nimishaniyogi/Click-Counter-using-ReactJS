ea778fd0e385b41e4d20a54839fa98bed4666404
nimishaniyogi <nimishaniyogi@gmail.com>
2018-08-05 15:46:21 -0500

    react project added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div onClick = {this.props.onClick}>
+        This div is clicked {this.props.clicks} times.
+      </div>
+
+    );
+  }
+}
+
+export default App;
